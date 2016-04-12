7bdff551893a7c6ce8e1f98b611af582b6d623fa
kickscar <kickscar@gmail.com>
2016-04-12 15:45:14 +0900

    1st commit

---
+++ WebContent/hello.js
@@ -0,0 +1,6 @@
+document.write("<strong>Hello World</strong>");
+//alert("Hello World");
+console.log( "Hello World" );
+console.error( "error!!!" );
+console.warn( "warning!!!" );
+console.info( "info......" );
